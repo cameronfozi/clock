ccb60f51164f95fa5f05be3f98939fb7666f1807
Cameron Fozi <cameronfozi@berkeley.edu>
2025-01-08 23:32:32 -0800

    add 4px margin

--- sketch.js
+++ sketch.js
@@ -3,8 +3,9 @@ var delayMinute;
 
 // setup() is called once at page-load
 function setup() {
+  
     delayMinute = minute();
-    createCanvas(396, 148);
+    createCanvas(388, 148);
     colorMode(RGB, 255, 255, 255);
     setBackground();
 
@@ -13,74 +14,75 @@ function setup() {
 // setBackground() sets the clock's background
 function setBackground() {
 
-    let myColor = color(240, 240, 240);
+    let myColor = color(200, 200, 200);
     background(myColor);
   
-    strokeWeight(2);
-    line(248, 0, 248, 148);
-    line(100, 0, 100, 148);
-  
     strokeWeight(0.5);
-    line(100, 74, 396, 74);
-    line(322, 0, 322, 148);
-    line(174, 0, 174, 148);
-   
-    strokeWeight(1);
-  
+    line(170, 4, 170, 144);
+    line(314, 4, 314, 144);
+    line(100, 74, 240, 74);
+    line(244, 74, 384, 74);
+     
 }
 
 // setSecond() sets the second hand
 function setSecond(sec) {
 
-  secondCompletion = map(sec, 0, 59, 0, 148 * 148)
-  fullSecondSquares = Math.floor(secondCompletion / ((148 * 148)/4))
+  squareLength = 140
+  squareArea = squareLength ** 2
+  
+  secondCompletion = map(sec, 0, 59, 0, squareArea)
+  fullSecondSquares = Math.floor(secondCompletion / (squareArea/4))
   
   if (fullSecondSquares == 0) {
-    square(248, 0, secondCompletion ** (0.5));
+    square(244, 4, secondCompletion ** (0.5));
   }
 
   if (fullSecondSquares == 1) {
-    square(248, 0, 74);
-    square(322, 0, (secondCompletion-((148 * 148)/4)) ** (0.5));
+    square(244, 4, 70);
+    square(314, 4, (secondCompletion-(squareArea/4)) ** (0.5));
   }
 
   if (fullSecondSquares == 2) {
-    rect(248, 0, 148, 74);
-    square(322, 74, (secondCompletion-((148 * 148)/2)) ** (0.5));
+    rect(244, 4, 140, 70);
+    square(314, 74, (secondCompletion-(squareArea/2)) ** (0.5));
   }
 
   if (fullSecondSquares == 3) {
-    rect(248, 0, 148, 74);
-    square(322, 74, 74);
-    square(248, 74, (secondCompletion-(3*(148 * 148)/4)) ** (0.5));
+    rect(244, 4, 140, 70);
+    square(314, 74, 70);
+    square(244, 74, (secondCompletion-(3*squareArea/4)) ** (0.5));
   }
 
 }
 
 // setMinute() sets the minute hand
 function setMinute(secs) {
+  
+    squareLength = 140
+    squareArea = squareLength ** 2
 
-    minuteCompletion= map(secs, 0, 3599, 0, 148 * 148)
-    fullMinuteSquares = Math.floor(minuteCompletion / ((148 * 148)/4))
+    minuteCompletion= map(secs, 0, 3599, 0, squareArea)
+    fullMinuteSquares = Math.floor(minuteCompletion / (squareArea/4))
     
     if (fullMinuteSquares == 0) {
-      square(100, 0, minuteCompletion ** (0.5));
+      square(100, 4, minuteCompletion ** (0.5));
     }
   
     if (fullMinuteSquares == 1) {
-      square(100, 0, 74);
-      square(174, 0, (minuteCompletion-((148 * 148)/4)) ** (0.5));
+      square(100, 4, 70);
+      square(170, 4, (minuteCompletion-(squareArea/4)) ** (0.5));
     }
   
     if (fullMinuteSquares == 2) {
-      rect(100, 0, 148, 74);
-      square(174, 74, (minuteCompletion-((148 * 148)/2)) ** (0.5));
+      rect(100, 4, 140, 70);
+      square(170, 74, (minuteCompletion-(squareArea/2)) ** (0.5));
     }
   
     if (fullMinuteSquares == 3) {
-      rect(100, 0, 148, 74);
-      square(174, 74, 74);
-      square(100, 74, (minuteCompletion-(3*(148 * 148)/4)) ** (0.5));
+      rect(100, 4, 140, 70);
+      square(174, 74, 70);
+      square(100, 74, (minuteCompletion-(3*squareArea/4)) ** (0.5));
     } 
 
 }
@@ -96,7 +98,7 @@ function setHour(min, hour) {
       
       if (hour <= 0) {
         
-        fill(240, 240, 240);
+        fill(200, 200, 200);
         square(4 + sq2 * 24, wdth, 20);
         fill(0, 0, 0);
         
@@ -127,8 +129,8 @@ function checkPrint(min) {
   
   if (min != delayMinute) {
 
-    console.log(min);
     setBackground();
+    console.log(min);
     delayMinute = min;
 
   }
@@ -138,14 +140,14 @@ function checkPrint(min) {
 // draw() is called 60 times per second
 function draw() {
   
-    let hr = hour();
     let min = minute();
+    let hr = hour();
     let sec = second();
+    checkPrint(min);
   
     fill(0, 0, 0);
     setSecond(sec);
     setMinute(sec + min * 60);
     setHour(min, hr);
-    checkPrint(min);
   
 }
